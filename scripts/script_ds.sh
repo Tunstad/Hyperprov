7dca0639753b3ed05fdeaadc1740949158766bdc
Petter Tunstad <Pettertunstad@gmail.com>
2019-01-30 14:52:04 +0100

    Updated images to HLF 1.1.0 and baseimages to 0.4.6

--- scripts/script_ds.sh
+++ scripts/script_ds.sh
@@ -20,10 +20,10 @@ ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrga
 #Prevents "Minimum memory limit allowed is 4MB" error on low RAM devices (like RasPi)
 CORE_VM_DOCKER_HOSTCONFIG_MEMORY=536870912
 # Sets the default images to use my build for the ARM architecture
-CORE_CHAINCODE_BUILDER=hyperledger/fabric-ccenv:x86_64-1.0.4 
-CORE_CHAINCODE_GOLANG=hyperledger/fabric-baseos:x86_64-0.3.2 
-CORE_CHAINCODE_CAR=hyperledger/fabric-baseos:x86_64-0.3.2 
-CORE_CHAINCODE_JAVA=hyperledger/fabric-javaenv::x86_64-1.0.4 
+CORE_CHAINCODE_BUILDER=hyperledger/fabric-ccenv:x86_64-1.1.0 
+CORE_CHAINCODE_GOLANG=hyperledger/fabric-baseos:x86_64-0.4.6 
+CORE_CHAINCODE_CAR=hyperledger/fabric-baseos:x86_64-0.4.6 
+CORE_CHAINCODE_JAVA=hyperledger/fabric-javaenv::x86_64-1.1.0 
 
 echo "Channel name : "$CHANNEL_NAME
 
