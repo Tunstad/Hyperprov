4d718e9d3b04c9ef493fdd5d95f21cc662847a50
Petter Tunstad <Pettertunstad@gmail.com>
2019-01-31 11:14:52 +0100

    Updated binaries for HLF version 1.3.0

--- bin/get-docker-images.sh
+++ bin/get-docker-images.sh
@@ -7,18 +7,21 @@
 
 # set the default Docker namespace and tag
 DOCKER_NS=hyperledger
-ARCH=x86_64
-VERSION=1.1.0
-BASE_DOCKER_TAG=x86_64-0.4.6
+ARCH=amd64
+VERSION=1.3.0
+BASE_DOCKER_TAG=amd64-0.4.13
 
 # set of Hyperledger Fabric images
-FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-javaenv fabric-kafka fabric-zookeeper \
-fabric-couchdb fabric-tools)
+FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-tools)
 
 for image in ${FABRIC_IMAGES[@]}; do
   echo "Pulling ${DOCKER_NS}/$image:${ARCH}-${VERSION}"
   docker pull ${DOCKER_NS}/$image:${ARCH}-${VERSION}
 done
 
-echo "Pulling ${DOCKER_NS}/fabric-baseos:${BASE_DOCKER_TAG}"
-docker pull ${DOCKER_NS}/fabric-baseos:${BASE_DOCKER_TAG}
+THIRDPARTY_IMAGES=(fabric-kafka fabric-zookeeper fabric-couchdb fabric-baseos)
+
+for image in ${THIRDPARTY_IMAGES[@]}; do
+  echo "Pulling ${DOCKER_NS}/$image:${BASE_DOCKER_TAG}"
+  docker pull ${DOCKER_NS}/$image:${BASE_DOCKER_TAG}
+done
