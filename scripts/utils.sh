268a2bf75f0c60652476a408d60f36113be0f760
Petter Tunstad <Pettertunstad@gmail.com>
2019-02-26 14:51:18 +0100

    Adding pointers/location to cc, json objects returned8

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -219,7 +219,7 @@ chaincodeQuery () {
     sleep $DELAY
     echo "Attempting to Query peer${PEER}.org${ORG} ...$(($(date +%s) - starttime)) secs"
     set -x
-    peer chaincode query -C $CHANNEL_NAME -n myccds -c '{"Args":["get","c"]}' >&log.txt
+    peer chaincode query -C $CHANNEL_NAME -n myccds -c '{"Args":["checkhash","c"]}' >&log.txt
     res=$?
     set +x
     test $res -eq 0 && VALUE=$(cat log.txt | awk '/Query Result/ {print $NF}')
